5f5006aea554dd7e76d52acc1e63db58fcba3f63
eviistdl <stadelevelin@gmail.com>
2023-11-30 16:12:45 +0100

    add alert

--- Aufgabe4/script.js
+++ Aufgabe4/script.js
@@ -3,27 +3,27 @@ let SurviveYourDay = prompt('Überlebe den Tag: Du bekommst einen Anruf, in dem 
 
 
 if (SurviveYourDay == "Keller") {
-   let answerEins = prompt("ENDE. Du fällst die Treppe runter und stirbst.");
+   alert("ENDE. Du fällst die Treppe runter und stirbst.");
 
 } else if (SurviveYourDay == "Uni") {
     let answerZwei = prompt('Dein Tag verläuft ohne Probleme. Doch auf dem Rückweg hörst du etwas im Gebüsch. Was tust du:"nachschauen" oder "rennen"');
     if (answerZwei == "nachschauen"){
-        let optionEins = prompt("Du schlüpfst hinter das Gebüsch, aber findest dort nichts. Gerade als du zurück auf den Weg willst, merkst du, dass du einem außer Kontrolle geratenen Auto ausgewichen bist. Du kommst unversehrt zuhause an. Glückwunsch, du hast überlebt!");
+        alert("Du schlüpfst hinter das Gebüsch, aber findest dort nichts. Gerade als du zurück auf den Weg willst, merkst du, dass du einem außer Kontrolle geratenen Auto ausgewichen bist. Du kommst unversehrt zuhause an. Glückwunsch, du hast überlebt!");
     }
     else if (answerZwei == "rennen"){
-        let optionZwei = prompt("Du rennst so schnell du kannst und lässt dabei das Gebüsch nicht aus den Augen. Dabei übersiehst du ein Auto, wirst angefahren und stirbst :(");
+        alert("Du rennst so schnell du kannst und lässt dabei das Gebüsch nicht aus den Augen. Dabei übersiehst du ein Auto, wirst angefahren und stirbst :(");
     }
 
 } else if (SurviveYourDay == "mein Zimmer") {
     let answerThree = prompt('Du sitzt den ganzen Tag unter deiner Decke. Irgendwann hörst du draußen etwas, was tust du: "nachschauen" oder "verstecken"')
     if (answerThree == "nachschauen"){
-        let optionEins = prompt("Du trittst vor die Tür und schaust dich um. Plötzlich bemerkst du was das Geräusch war. Du schaffst es nicht zu entkommen, wirst von einer Dachlawine begraben und stirbst :() ")
+        alert("Du trittst vor die Tür und schaust dich um. Plötzlich bemerkst du was das Geräusch war. Du schaffst es nicht zu entkommen, wirst von einer Dachlawine begraben und stirbst :() ")
     }
     else if (answerThree == "verstecken"){
-        let optionZwei = prompt("Wie erstarrt sitzt du weiterhin unter deiner Decke. Eine Dachlawine lässt dich aufschrecken, doch der restliche Tag bleibt ereignislos. Glückwunch, du hast überlebt!:)")
+        alert("Wie erstarrt sitzt du weiterhin unter deiner Decke. Eine Dachlawine lässt dich aufschrecken, doch der restliche Tag bleibt ereignislos. Glückwunch, du hast überlebt!:)")
     }
 
 } else {
-   let error = prompt("Error. Load again.");
+   alert("Error. Load again.");
 } 
  
